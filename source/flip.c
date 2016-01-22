5f460cb4b30b125341635aed62272c1251260ce5
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-01-22 22:43:45 +0100

    some testing

--- source/flip.c
+++ source/flip.c
@@ -4,10 +4,10 @@
 int pbm_image_flip(PbmImage* image){
   int status = RET_PBM_ERROR;
 
-  size_t count =  (image->height * image->width) + 1;
+  size_t count =  (image->height * image->width);
 
   //(c) Gerhard Seuchter, Folien
-    for(int i = 1; i <= count/2; i++){
+    for(int i = 0; i <= count/2; i++){
 		char tmp = image->data[i];
 		image->data[i] = image->data[count - 2 - i];
 		image->data[count - 2 - i] = tmp;

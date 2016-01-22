cf24ec041b530fdb53d3b4022a4acff2da2466df
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-01-22 22:50:28 +0100

    Once more

--- source/img.c
+++ source/img.c
@@ -58,7 +58,7 @@ PbmImage* pbm_image_load_from_stream(FILE* stream, int* error){
     //data allocation
     size_t binarysize = (image->width * image->height);
     image->data = malloc(binarysize);
-    printf("Der String '%s'\n", fgets(bits, 2, stream));
+    //printf("Der String '%s'\n", fgets(bits, 2, stream));
     char test[1];
     fread(test, 1, 1, stream);
     fread(image->data, binarysize, 1, stream);
